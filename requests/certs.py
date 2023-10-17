23c2c4a496b34237f2f2211f3dca8e7a2d1a17a8
Tomáš Hrnčiar <thrnciar@redhat.com>
2023-10-17 10:44:42 +0200

    feat(requests/certs.py): use system certificate store
    Derived from Fedora's system cert patch. [^1]
    
    [^1]: https://src.fedoraproject.org/rpms/python-requests/blob/46169880a4284a332635d6552176a4dc1a9441f3/f/system-certs.patch
    
    Co-authored-by: Lumir Balhar <lbalhar@redhat.com>

--- requests/certs.py
+++ requests/certs.py
@@ -10,8 +10,14 @@ only one — the one from the certifi package.
 If you are packaging Requests, e.g., for a Linux distribution or a managed
 environment, you can change the definition of where() to return a separately
 packaged CA bundle.
+
+This AOSC-patched package returns "/etc/ssl/ca-bundle.crt" provided by the
+ca-certs package.
 """
-from certifi import where
+
+def where():
+    """Return the absolute path to the system CA bundle."""
+    return '/etc/ssl/ca-bundle.crt'
 
 if __name__ == "__main__":
     print(where())
